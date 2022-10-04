79dace5ecf3b80ad0e18f3b08fb76ff5d1215e1e
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2022-10-04 11:47:09 -0300

    :tada: feat: add initiated project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 function App() {
 	return (
-		<div className="max-w-7xl mx-auto">
-			<h1 className="font-bold text-xl">Hola Mundo</h1>
+		<div>
+			<h1 className="font-bold text-xl">NavBar</h1>
+			<div className="max-w-7xl mx-auto px-4">
+				<h1 className="font-bold text-xl">Container</h1>
+			</div>
 		</div>
 	)
 }
