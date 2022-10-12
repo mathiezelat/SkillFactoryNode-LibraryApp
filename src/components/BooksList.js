e7c84b6ea5d405d97b2cd6ea5b5ed1e4009b089d
rolando duarte <rolandoduarte83@gmail.com>
2022-10-11 22:12:15 -0300

    :sparkles: Uploading RTK project, components Details.js, BooksList.js, BookForm.js, Home.js, bookSlice.js, store.js

---
+++ src/components/BooksList.js
@@ -0,0 +1,47 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteBook } from '../features/books/booksSlice';
+import { Link } from 'react-router-dom';
+
+
+function BooksList() {
+    const books = useSelector((state) => state.books);
+    const dispatch = useDispatch();
+
+    const handleDelete = (id) => {
+        dispatch(deleteBook(id));
+    };
+
+    return (
+        <article className="card-book-list">
+            <div>
+                <h3 className='count'>Books: {books.length}</h3>
+                {books.map((book) => {
+                    return (
+                        <div className="headings">
+                            <section className="book-container">
+                                <div key={book.id} >
+                                    <h2>{book.title}</h2>
+                                    <h3>{book.author}</h3>
+                                    <div className="image">
+                                        <img
+                                            src={book.url}
+                                            alt="imag-card"
+                                        />
+                                    </div>
+                                    <Link to={`/update-book/${book.id}`}>Update</Link>
+                                    <Link to={`/detail-book/${book.id}`}>Details</Link>
+                                    <button onClick={() => handleDelete(book.id)}>
+                                        Delete
+                                    </button>
+                                </div>
+                            </section>
+                        </div>
+                    );
+                })}
+
+            </div>
+        </article >
+    )
+}
+
+export default BooksList;
