d4783629adb83e66c5bd4595e08b72bfb7ba4447
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2022-10-16 01:59:48 -0300

    :sparkles: feat: add svg home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,20 @@
+// import BooksIcon from '../assets/icons/books.png'
+import SvgBooks from '../assets/illustrations/SvgBooks'
+
 function Home() {
 	return (
-		<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-			<div className="flex flex-col justify-center items-center pt-20">
+		<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative overflow-hidden">
+			<div className="flex flex-col justify-center items-center pt-20 ">
 				<div>
-					<h1 className="text-slate-900 font-semibold text-4xl tracking-tight text-center">
-						Library App
-					</h1>
-					<p className="mt-2 text-slate-600 text-center">
-						Library of your dreams
-					</p>
+					<div className="absolute left-0 right-0">
+						<h1 className="text-slate-900 font-semibold text-4xl tracking-tight text-center">
+							Library App
+						</h1>
+						<p className="mt-2 text-slate-600 text-center">
+							Library of your dreams
+						</p>
+					</div>
+					<SvgBooks />
 				</div>
 			</div>
 		</div>
