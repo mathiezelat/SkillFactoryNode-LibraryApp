aa83e34224e6c693b6933a8c345f20ae4502d12d
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2022-10-17 09:35:19 -0300

    :lipstick: style: some style modifications sign up

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -19,9 +19,11 @@ const SignUp = () => {
 		<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 			<div className="py-6 px-4 sm:px-0 text-center">
 				<h3 className="text-3xl font-medium leading-6 text-gray-900">
-					Sign Up
+					Sign up
 				</h3>
-
+				<p className="mt-1 text-lg text-gray-600">
+					Sign up to your favorite page ;)
+				</p>
 			</div>
 			<form
 				onSubmit={handleSubmit(onSubmit)}
@@ -29,81 +31,71 @@ const SignUp = () => {
 			>
 				<div className="flex flex-col gap-4">
 					<div className="flex flex-col">
-
 						<label
 							htmlFor="text"
 							className="block text-sm font-medium text-gray-700"
 						>
-							First Name
+							First name
 						</label>
 						<input
 							id="firstName"
 							type="text"
 							className="mt-1 block w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
-							{...register("firstName", {
+							{...register('firstName', {
 								required: true,
 								maxLength: 10,
 							})}
 						/>
-					</div>
-
-					<div
-						className={`${errors.firstName ? 'visible' : 'invisible'
+						<div
+							className={`${
+								errors.firstName ? 'visible' : 'invisible'
 							}`}
-					>
-						{errors.firstName?.type === 'required' && (
-							<p className="text-xs absolute text-red-500">
-								First Name is required
-							</p>
-						)}
-						{errors.firstName?.type === 'maxLength' && (
-							<p className="text-xs absolute text-red-500">
-								You must enter the required digits
-							</p>
-						)}
+						>
+							{errors.firstName?.type === 'required' && (
+								<p className="text-xs absolute text-red-500">
+									First Name is required
+								</p>
+							)}
+							{errors.firstName?.type === 'maxLength' && (
+								<p className="text-xs absolute text-red-500">
+									You must enter the required digits
+								</p>
+							)}
+						</div>
 					</div>
-				</div>
-				<br />
-				<div className="flex flex-col gap-4">
-
 					<div className="flex flex-col">
-
 						<label
 							htmlFor="text"
 							className="block text-sm font-medium text-gray-700"
 						>
-							Last Name
+							Last name
 						</label>
 						<input
 							id="lastName"
 							type="text"
 							className="mt-1 block w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
-							{...register("lastName", {
+							{...register('lastName', {
 								required: true,
 								maxLength: 10,
 							})}
 						/>
-											<div
-						className={`${errors.lastName ? 'visible' : 'invisible'
+						<div
+							className={`${
+								errors.lastName ? 'visible' : 'invisible'
 							}`}
-					>
-						{errors.lastName?.type === 'required' && (
-							<p className="text-xs absolute text-red-500">
-								Last Name Name is required
-							</p>
-						)}
-						{errors.lastName?.type === 'maxLength' && (
-							<p className="text-xs absolute text-red-500">
-								You must enter the required digits
-							</p>
-						)}
+						>
+							{errors.lastName?.type === 'required' && (
+								<p className="text-xs absolute text-red-500">
+									Last Name Name is required
+								</p>
+							)}
+							{errors.lastName?.type === 'maxLength' && (
+								<p className="text-xs absolute text-red-500">
+									You must enter the required digits
+								</p>
+							)}
+						</div>
 					</div>
-					</div>
-				</div>
-				<br />
-
-				<div className="flex flex-col gap-4">
-
 					<div className="flex flex-col">
 						<label
 							htmlFor="email"
@@ -121,8 +113,9 @@ const SignUp = () => {
 							})}
 						/>
 						<div
-							className={`${errors.email ? 'visible' : 'invisible'
-								}`}
+							className={`${
+								errors.email ? 'visible' : 'invisible'
+							}`}
 						>
 							{errors.email?.type === 'required' && (
 								<p className="text-xs absolute text-red-500">
@@ -136,7 +129,6 @@ const SignUp = () => {
 							)}
 						</div>
 					</div>
-					<br />
 					<div className="flex flex-col">
 						<label
 							htmlFor="password"
@@ -154,8 +146,9 @@ const SignUp = () => {
 							})}
 						/>
 						<div
-							className={`${errors.password ? 'visible' : 'invisible'
-								}`}
+							className={`${
+								errors.password ? 'visible' : 'invisible'
+							}`}
 						>
 							{errors.password?.type === 'required' && (
 								<p className="text-xs absolute text-red-500">
@@ -169,7 +162,6 @@ const SignUp = () => {
 							)}
 						</div>
 					</div>
-					<br />
 					<div className="flex flex-col">
 						<label
 							htmlFor="password"
@@ -187,8 +179,9 @@ const SignUp = () => {
 							})}
 						/>
 						<div
-							className={`${errors.password ? 'visible' : 'invisible'
-								}`}
+							className={`${
+								errors.password ? 'visible' : 'invisible'
+							}`}
 						>
 							{errors.password?.type === 'required' && (
 								<p className="text-xs absolute text-red-500">
@@ -204,31 +197,29 @@ const SignUp = () => {
 					</div>
 
 					<input
-						to="/login"
 						type="submit"
-						value="Log in"
+						value="Sign up"
 						className="cursor-pointer self-center inline-flex justify-center rounded-lg border border-transparent bg-blue-600 py-2 px-4 font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:blue-indigo-500 focus:ring-offset-2"
 					/>
-					
-					
 
 					<div className="flex justify-center items-center gap-1">
 						<p className="text-sm text-gray-700">
-							Don't have an account?
+							Have an account?
 						</p>
 						<Link
-							to="/signup"
+							to="/login"
 							className="text-sm text-blue-500 hover:text-blue-400"
 						>
-							Sign up
+							Log in
 						</Link>
 					</div>
 
 					<div
-						className={`${Object.keys(errors).length > 0
-							? 'visible'
-							: 'invisible'
-							}`}
+						className={`${
+							Object.keys(errors).length > 0
+								? 'visible'
+								: 'invisible'
+						}`}
 					>
 						<p className="text-sm text-center text-red-500">
 							There are errors, check form.
@@ -241,4 +232,3 @@ const SignUp = () => {
 }
 
 export default SignUp
-
