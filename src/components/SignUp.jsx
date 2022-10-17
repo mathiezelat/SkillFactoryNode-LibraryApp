961dc96ef113f2fdcc4cbb9407bee0382903135c
Rolando Duarte <rolandoduarte83@gmail.com>
2022-10-17 01:13:23 -0300

    :bug: Sign up revew and edited, deleted unused folders

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,178 +1,229 @@
-import React, { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Input from './ComponentInput.js'
-import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
-import {
-	Form,
-	Label,
-	ContainerTerms,
-	CenteredButtonContainer,
-	Button,
-	SuccessMessage,
-	ErrorMessage,
-} from '../elements/Forms'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
-	const [usuario, cambiarUsuario] = useState({ campo: '', valido: null })
-	const [nombre, cambiarNombre] = useState({ campo: '', valido: null })
-	const [password, cambiarPassword] = useState({ campo: '', valido: null })
-	const [password2, cambiarPassword2] = useState({ campo: '', valido: null })
-	const [correo, cambiarCorreo] = useState({ campo: '', valido: null })
-	const [telefono, cambiarTelefono] = useState({ campo: '', valido: null })
-	const [terminos, cambiarTerminos] = useState(false)
-	const [formValido, cambiarFormValido] = useState(null)
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { errors },
+	} = useForm()
 
-	const exprersiones = {
-		usuario: /^[a-zA-Z0-9_-]{4,16}$/, //letras, numeros, guion, guion_bajo.
-		nombre: /^[a-zA-ZÀ-Ÿ\s]{1,40}$/, //letras y espacios, pueden llevar guion.
-		password: /^.{4,12}$/, //4 a 12 digitos.
-		correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-		telefono: /^\d{7,14}$/, //7 a 14 numeros.
-	}
+	const onSubmit = data => {
+		console.log(data)
 
-	const validarPassword2 = () => {
-		if (password.campo.length > 0) {
-			if (password.campo !== password2.campo) {
-				cambiarPassword2(prevState => {
-					return { ...prevState, valido: 'false' }
-					//console.log('las contraseñas NO son iguales')
-				})
-			} else {
-				cambiarPassword2(prevState => {
-					return { ...prevState, valido: 'true' }
-				})
-				//console.log('las contraseñas SI son igualitassss')
-			}
-		}
-	}
-
-	const onChangeTerminos = e => {
-		cambiarTerminos(e.target.checked)
-	}
-
-	const onSubmit = e => {
-		e.preventDefault()
-
-		if (
-			usuario.valido === 'true' &&
-			nombre.valido === 'true' &&
-			password.valido === 'true' &&
-			password2.valido === 'true' &&
-			correo.valido === 'true' &&
-			telefono.valido === 'true' &&
-			terminos
-		) {
-			cambiarFormValido(true)
-			cambiarUsuario({ campo: '', valido: '' }) //reiniciamos los valores - formulario a vacio
-			cambiarNombre({ campo: '', valido: null }) //reiniciamos los valores - formulario a vacio
-			cambiarPassword({ campo: '', valido: null }) //reiniciamos los valores - formulario a vacio
-			cambiarPassword2({ campo: '', valido: 'null' }) //reinireiniciamos los valores -ciar formulario a vacio
-			cambiarCorreo({ campo: '', valido: null }) //reiniciamos los valores - formulario a vacio
-			cambiarTelefono({ campo: '', valido: null }) //reiniciamos los valores - formulario a vacio
-		} else {
-			cambiarFormValido(false)
-		}
+		reset()
 	}
 
 	return (
-		<main className="contac">
-			<Form action="" onSubmit={onSubmit}>
-				<Input
-					estado={usuario}
-					cambiarEstado={cambiarUsuario}
-					type="text"
-					label="User"
-					placeholder="John Doe"
-					name="usuario"
-					legendError="The user must be from 4 to 16 digits and can only contain numbers, letters and underscore."
-					regularPhrase={exprersiones.usuario}
-				/>
+		<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+			<div className="py-6 px-4 sm:px-0 text-center">
+				<h3 className="text-3xl font-medium leading-6 text-gray-900">
+					Sign Up
+				</h3>
 
-				<Input
-					estado={nombre}
-					cambiarEstado={cambiarNombre}
-					type="text"
-					label="Name"
-					placeholder="John Doe"
-					name="nombre"
-					legendError="The name must contain only letters and space."
-					regularPhrase={exprersiones.nombre}
-				/>
+			</div>
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className="py-2 mx-auto max-w-lg"
+			>
+				<div className="flex flex-col gap-4">
+					<div className="flex flex-col">
 
-				<Input
-					estado={password}
-					cambiarEstado={cambiarPassword}
-					type="password"
-					label="Password"
-					name="password 1"
-					placeholder="***********"
-					legendError="The password must contain from 4 to 12 digits."
-					regularPhrase={exprersiones.password}
-				/>
-
-				<Input
-					estado={password2}
-					cambiarEstado={cambiarPassword2}
-					type="password"
-					label="Repeat password"
-					name="password2"
-					placeholder="***********"
-					legendError="Both passwords must be the same."
-					funcion={validarPassword2}
-				/>
-
-				<Input
-					estado={correo}
-					cambiarEstado={cambiarCorreo}
-					type="email"
-					label="Email"
-					placeholder="john_Doe@gmail.com"
-					name="correo"
-					legendError="Email can only contain letters, numbers, periods, hyphens and underscores."
-					regularPhrase={exprersiones.correo}
-				/>
-				<Input
-					estado={telefono}
-					cambiarEstado={cambiarTelefono}
-					type="text"
-					label="Phone"
-					placeholder="1234567890"
-					name="telefono"
-					legendError="The phone can only contain numbers and the maximum is 15 digits."
-					regularPhrase={exprersiones.telefono}
-				/>
-
-				<ContainerTerms>
-					<Label>
+						<label
+							htmlFor="text"
+							className="block text-sm font-medium text-gray-700"
+						>
+							First Name
+						</label>
 						<input
-							type="checkbox"
-							name="terms"
-							id="terms"
-							checked={terminos}
-							onChange={onChangeTerminos}
+							id="firstName"
+							type="text"
+							className="mt-1 block w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+							{...register("firstName", {
+								required: true,
+								maxLength: 10,
+							})}
 						/>
-						I accept the terms and conditions
-					</Label>
-				</ContainerTerms>
-				{formValido === false && (
-					<ErrorMessage>
-						<p>
-							<FontAwesomeIcon icon={faExclamationTriangle} />
-							<b>Error:</b> Please fill in this field is required.
+					</div>
+
+					<div
+						className={`${errors.firstName ? 'visible' : 'invisible'
+							}`}
+					>
+						{errors.firstName?.type === 'required' && (
+							<p className="text-xs absolute text-red-500">
+								First Name is required
+							</p>
+						)}
+						{errors.firstName?.type === 'maxLength' && (
+							<p className="text-xs absolute text-red-500">
+								You must enter the required digits
+							</p>
+						)}
+					</div>
+				</div>
+				<br />
+				<div className="flex flex-col gap-4">
+
+					<div className="flex flex-col">
+
+						<label
+							htmlFor="text"
+							className="block text-sm font-medium text-gray-700"
+						>
+							Last Name
+						</label>
+						<input
+							id="lastName"
+							type="text"
+							className="mt-1 block w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+							{...register("lastName", {
+								required: true,
+								maxLength: 10,
+							})}
+						/>
+					</div>
+				</div>
+				<br />
+
+				<div className="flex flex-col gap-4">
+
+					<div className="flex flex-col">
+						<label
+							htmlFor="email"
+							className="block text-sm font-medium text-gray-700"
+						>
+							Email
+						</label>
+						<input
+							id="email"
+							type="email"
+							className="mt-1 block w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+							{...register('email', {
+								required: true,
+								pattern: /\S+@\S+\.\S+/,
+							})}
+						/>
+						<div
+							className={`${errors.email ? 'visible' : 'invisible'
+								}`}
+						>
+							{errors.email?.type === 'required' && (
+								<p className="text-xs absolute text-red-500">
+									Email is required
+								</p>
+							)}
+							{errors.email?.type === 'pattern' && (
+								<p className="text-xs absolute text-red-500">
+									Entered value does not match email format
+								</p>
+							)}
+						</div>
+					</div>
+					<br />
+					<div className="flex flex-col">
+						<label
+							htmlFor="password"
+							className="block text-sm font-medium text-gray-700"
+						>
+							Password
+						</label>
+						<input
+							id="password"
+							type="password"
+							className="mt-1 block w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+							{...register('password', {
+								required: true,
+								minLength: 8,
+							})}
+						/>
+						<div
+							className={`${errors.password ? 'visible' : 'invisible'
+								}`}
+						>
+							{errors.password?.type === 'required' && (
+								<p className="text-xs absolute text-red-500">
+									Password is required
+								</p>
+							)}
+							{errors.password?.type === 'minLength' && (
+								<p className="text-xs absolute text-red-500">
+									Password must contain more than 8 digits
+								</p>
+							)}
+						</div>
+					</div>
+					<br />
+					<div className="flex flex-col">
+						<label
+							htmlFor="password"
+							className="block text-sm font-medium text-gray-700"
+						>
+							Confirm Password
+						</label>
+						<input
+							id="password"
+							type="password"
+							className="mt-1 block w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+							{...register('password', {
+								required: true,
+								minLength: 8,
+							})}
+						/>
+						<div
+							className={`${errors.password ? 'visible' : 'invisible'
+								}`}
+						>
+							{errors.password?.type === 'required' && (
+								<p className="text-xs absolute text-red-500">
+									Password is required
+								</p>
+							)}
+							{errors.password?.type === 'minLength' && (
+								<p className="text-xs absolute text-red-500">
+									Password must contain more than 8 digits
+								</p>
+							)}
+						</div>
+					</div>
+
+					<input
+						to="/login"
+						type="submit"
+						value="Log in"
+						className="cursor-pointer self-center inline-flex justify-center rounded-lg border border-transparent bg-blue-600 py-2 px-4 font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:blue-indigo-500 focus:ring-offset-2"
+					/>
+					
+					
+
+					<div className="flex justify-center items-center gap-1">
+						<p className="text-sm text-gray-700">
+							Don't have an account?
 						</p>
-					</ErrorMessage>
-				)}
-				<CenteredButtonContainer>
-					<Button type="submit">Send</Button>
-					{formValido === true && (
-						<SuccessMessage>
-							The form was sent successfully!
-						</SuccessMessage>
-					)}
-				</CenteredButtonContainer>
-			</Form>
-		</main>
+						<Link
+							to="/signup"
+							className="text-sm text-blue-500 hover:text-blue-400"
+						>
+							Sign up
+						</Link>
+					</div>
+
+					<div
+						className={`${Object.keys(errors).length > 0
+							? 'visible'
+							: 'invisible'
+							}`}
+					>
+						<p className="text-sm text-center text-red-500">
+							There are errors, check form.
+						</p>
+					</div>
+				</div>
+			</form>
+		</div>
 	)
 }
 
 export default SignUp
+
